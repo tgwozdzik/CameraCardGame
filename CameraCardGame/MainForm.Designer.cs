626f504cd12a810532663954a7515783652d3a8b
Tomasz Gwoździk <tomasz.gwozdzik@outlook.com>
2016-03-28 11:36:31 +0200

    Capture video from android device

--- CameraCardGame/MainForm.Designer.cs
+++ CameraCardGame/MainForm.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.videoPlayer = new AForge.Controls.VideoSourcePlayer();
+            this.inputURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.showCameraVideo = new System.Windows.Forms.Button();
+            this.closeCameraVideo = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // videoPlayer
+            // 
+            this.videoPlayer.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.videoPlayer.Location = new System.Drawing.Point(12, 12);
+            this.videoPlayer.Name = "videoPlayer";
+            this.videoPlayer.Size = new System.Drawing.Size(348, 225);
+            this.videoPlayer.TabIndex = 0;
+            this.videoPlayer.Text = "videoPlayer";
+            this.videoPlayer.VideoSource = null;
+            // 
+            // inputURL
+            // 
+            this.inputURL.Location = new System.Drawing.Point(12, 273);
+            this.inputURL.Name = "inputURL";
+            this.inputURL.Size = new System.Drawing.Size(348, 20);
+            this.inputURL.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Video URL";
+            // 
+            // showCameraVideo
+            // 
+            this.showCameraVideo.Location = new System.Drawing.Point(189, 313);
+            this.showCameraVideo.Name = "showCameraVideo";
+            this.showCameraVideo.Size = new System.Drawing.Size(171, 41);
+            this.showCameraVideo.TabIndex = 3;
+            this.showCameraVideo.Text = "Show";
+            this.showCameraVideo.UseVisualStyleBackColor = true;
+            this.showCameraVideo.Click += new System.EventHandler(this.showCameraVideo_Click);
+            // 
+            // closeCameraVideo
+            // 
+            this.closeCameraVideo.Location = new System.Drawing.Point(108, 313);
+            this.closeCameraVideo.Name = "closeCameraVideo";
+            this.closeCameraVideo.Size = new System.Drawing.Size(75, 41);
+            this.closeCameraVideo.TabIndex = 4;
+            this.closeCameraVideo.Text = "Close";
+            this.closeCameraVideo.UseVisualStyleBackColor = true;
+            this.closeCameraVideo.Click += new System.EventHandler(this.closeCameraVideo_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(548, 366);
+            this.ClientSize = new System.Drawing.Size(373, 366);
+            this.Controls.Add(this.closeCameraVideo);
+            this.Controls.Add(this.showCameraVideo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputURL);
+            this.Controls.Add(this.videoPlayer);
             this.Name = "MainForm";
             this.Text = "CameraCardGame";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoPlayer;
+        private System.Windows.Forms.TextBox inputURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showCameraVideo;
+        private System.Windows.Forms.Button closeCameraVideo;
     }
 }
 
--- CameraCardGame/MainForm.cs
+++ CameraCardGame/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 using AForge.Video;
 using AForge.Video.DirectShow;
@@ -18,6 +19,57 @@ namespace CameraCardGame
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void showCameraVideo_Click(object sender, EventArgs e)
+        {
+            MJPEGStream mjpegStream = new MJPEGStream("http://" + inputURL.Text + "/mjpegfeed?640x480");
+
+            OpenVideoSource(mjpegStream);
+        }
+
+        private void OpenVideoSource(IVideoSource source)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            
+            CloseCurrentVideoSource();
+
+            videoPlayer.VideoSource = source;
+            videoPlayer.Start();
+
+            this.Cursor = Cursors.Default;
+        }
+
+        private void CloseCurrentVideoSource()
+        {
+            if (videoPlayer.VideoSource != null)
+            {
+                videoPlayer.SignalToStop();
+
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!videoPlayer.IsRunning)
+                        break;
+                    System.Threading.Thread.Sleep(100);
+                }
+
+                if (videoPlayer.IsRunning)
+                {
+                    videoPlayer.Stop();
+                }
+
+                videoPlayer.VideoSource = null;
+            }
+        }
+
+        private void closeCameraVideo_Click(object sender, EventArgs e)
+        {
+            CloseCurrentVideoSource();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseCurrentVideoSource();
         }
     }
 }
