70c8156881371a7261eb002cc9890068b7286f70
Tomasz Gwoździk <tomasz.gwozdzik@outlook.com>
2016-03-28 11:09:08 +0200

    Add video library

--- CameraCardGame/MainForm.cs
+++ CameraCardGame/MainForm.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using AForge.Video;
+using AForge.Video.DirectShow;
+
 namespace CameraCardGame
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
